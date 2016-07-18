43d6686cbe17aa4cf62dbea86f8b41c6d049d1b0
cnellington <caleb.n.ellington@gmail.com>
2016-07-17 22:55:35 -0500

    runs N10-N100 analysis for plotting

--- runner.py
+++ runner.py
@@ -5,6 +5,9 @@ file = "x_clustered_nt_pairs_over1read.txt"
 
 s = Stats(file)
 print(s.totalClusters())
-print(s.N50())
-print(s.n50())
-print(s.percent())
+print "N50: %s N90: %s" % (s.nN50(), s.nN90())
+print "nN50: %s nN90: %s\n" % (s.N50(), s.N90())
+print "[NValue, nNValue]"
+for x in range(10,100, 10):
+	print ("N%s"+str(s.nNValue(x))) % x
+	print(s.NValuePercent(x))
