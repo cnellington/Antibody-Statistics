793fcd2527652246ace4a112c4fcc298b640f3d0
cnellington <caleb.n.ellington@gmail.com>
2016-07-17 23:11:31 -0500

    added N100 testing

--- runner.py
+++ runner.py
@@ -8,6 +8,6 @@ print(s.totalClusters())
 print "N50: %s N90: %s" % (s.nN50(), s.nN90())
 print "nN50: %s nN90: %s\n" % (s.N50(), s.N90())
 print "[NValue, nNValue]"
-for x in range(10,100, 10):
+for x in range(10,101, 10):
 	print ("N%s"+str(s.nNValue(x))) % x
 	print(s.NValuePercent(x))
