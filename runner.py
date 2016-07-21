9ee073bd04f93226eb4e38a168e00e4535db6094
cnellington <caleb.n.ellington@gmail.com>
2016-07-21 13:45:17 -0500

    added plotting functionality and options

--- runner.py
+++ runner.py
@@ -5,11 +5,8 @@ file = "x_clustered_nt_pairs_over1read.txt"
 
 s = Stats(file)
 print(s.totalClusters())
-print "N50: %s N90: %s" % (s.nN50(), s.nN90())
-print "nN50: %s nN90: %s" % (s.N50(), s.N90())
-s.getAllNValues() #Speed up other functions
 print "[NValue, nNValue]"
 for x in range(10,101, 10):
 	print ("N%s"+str(s.nNValue(x))) % x
 	print(s.NValuePercent(x))
-s.NPlot()
+s.testPlot(nplot=True, littlenplot = True)
