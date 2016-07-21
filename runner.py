be1d4369f17ae058c94cb986ead13f5c0f78f896
cnellington <caleb.n.ellington@gmail.com>
2016-07-20 23:11:36 -0500

    demonstrates pre-calculation of N values, faster plotting, messages

--- runner.py
+++ runner.py
@@ -6,7 +6,8 @@ file = "x_clustered_nt_pairs_over1read.txt"
 s = Stats(file)
 print(s.totalClusters())
 print "N50: %s N90: %s" % (s.nN50(), s.nN90())
-print "nN50: %s nN90: %s\n" % (s.N50(), s.N90())
+print "nN50: %s nN90: %s" % (s.N50(), s.N90())
+s.getAllNValues() #Speed up other functions
 print "[NValue, nNValue]"
 for x in range(10,101, 10):
 	print ("N%s"+str(s.nNValue(x))) % x
