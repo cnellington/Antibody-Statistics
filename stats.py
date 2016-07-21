7bcac72b29519fd959fbcb5a007d68617b2d4284
cnellington <caleb.n.ellington@gmail.com>
2016-07-21 13:44:31 -0500

    Added little n analysis graph and double line plotting

--- stats.py
+++ stats.py
@@ -9,6 +9,7 @@ class Stats:
 	clusters = None #arange object
 	lengths = None #list object
 	nvalues = [] #stored Nvalues 0-100 to cut down on Nvalue usage/runtime (values added with NPlot() or getAllNValues())
+	#littlenvalues = [] #stored little n values
 	allvalues = False #has getAllNValues been run
 
 	def __init__(self, filename):
@@ -23,6 +24,9 @@ class Stats:
 		return self.clusters.count()
 
 
+
+
+#~~~~~~~~~~ Calculating N Value Data ~~~~~~~~~~~~~
 	#Returns Big N50 cluster analysis
 	#The number of clusters constituting the top 50 percent
 	#of the total mass of clusters
@@ -49,13 +53,25 @@ class Stats:
 		else:
 			return self.nvalues[int(n)]
 
-
 	def N50(self):
 		return self.NValue(50.0)
 
 	def N90(self):
 		return self.NValue(90.0)
 
+	#Store N values so NValues() doesn't have to keep being called
+	def getAllNValues(self):
+
+		print("\ncalculating all N Values, this may take a second...")
+
+		for x in range(0,101):
+			self.nvalues.append(self.NValue(x))
+		self.allvalues = True #Set allvalues to true, save time in other methods
+
+
+
+
+#~~~~~~~~~~ Calculating Little N Value Data ~~~~~~~~~~~~~
 	#Returns Little n50 cluster analysis
 	#The smallest value above the N50 threshold
 	def nNValue(self, x):
@@ -70,33 +86,15 @@ class Stats:
 		n = self.N90()
 		return [self.lengths[self.lengths>=n].count(), n]
 
-	def NPlot(self):
-		if(not self.allvalues):
-			self.getAllNValues()
-		xaxis = range(1,101)
+	def getAllLittleNValues(self):
+		littleNValues = []
+		for x in range(1,101):
+			littleNValues.append(self.nNValue(x)[1])
+		return littleNValues
 
-		buff = .05 #determines how close the plot is to the axes
-		xbuffer = int(xaxis[len(xaxis)-1]*buff) #x-axis buffer for clarity
-		ybuffer = int(self.nvalues[0]*buff) #y-axis buffer for clarity
-		
-		plt.ylabel("Cluster Size")
-		plt.xlabel("N value (smallest cluster above n percent mass)")
-		plt.plot(xaxis, self.nvalues[1:])
-		plt.axis([0-xbuffer, 100+xbuffer, 0-ybuffer, self.nvalues[0]+ybuffer ]) #plot axes, buffers added to both limits
-		
-		print("\nplotting graph...\n")
-		plt.show()
 
-		#finish
-		print "plot has been produced"
 
-	#Store N values so NValues() doesn't have to keep being called
-	def getAllNValues(self):
-		print("\ncalculating all N Values, this may take a second...\n")
-		for x in range(0,101):
-			self.nvalues.append(self.NValue(x))
-		self.allvalues = True #Set allvalues to true, save time in other methods
-
+#~~~~~~~~~~ Percent N Value Data ~~~~~~~~~~~~~
 	#Returns the percent above the N50 threshold showing
 	#data accuracy
 	def NValuePercent(self, n):
@@ -107,3 +105,90 @@ class Stats:
 
 		#return percentage
 		return float(littleN)/float(total)*100
+
+
+
+#~~~~~~~~~~ Graphing N Value Data ~~~~~~~~~~~~~
+	#Plot from N01 to N100
+	def NPlot(self):
+
+		#Produce and store necessary N values
+		if(not self.allvalues):
+			self.getAllNValues()
+
+		xaxis = range(1,101)
+
+		#uncomment if margins are needed for graph clarity
+		#buff = .05 #determines how close the plot is to the axes
+		#xbuffer = int(xaxis[len(xaxis)-1]*buff) #x-axis buffer for clarity
+		#ybuffer = int(self.nvalues[0]*buff) #y-axis buffer for clarity
+		
+		plt.ylabel("Cluster Size")
+		plt.xlabel("N value (smallest cluster above n percent mass)")
+		plt.plot(xaxis, self.nvalues[1:])
+		#plt.axis([0-xbuffer, 100+xbuffer, 0-ybuffer, self.nvalues[0]+ybuffer ]) #plot axes, buffers added to both limits
+		
+		print("\nplotting graph...")
+		plt.show()
+
+		#finish
+		print "plot has been produced"
+
+	#Plot from nN01 to nN100
+	def nNPlot(self):
+
+		#Produce and store necessary N values
+		if(not self.allvalues):
+			self.getAllNValues()
+
+		xaxis = range(1,101)
+
+		#Fetch all little n values
+		littlenvalues = self.getAllLittleNValues()
+		
+		
+		plt.ylabel("Number of Clusters Above NX")
+		plt.xlabel("N value")
+		plt.plot(xaxis, littlenvalues)
+		#plt.axis([0-xbuffer, 100+xbuffer, 0-ybuffer, littleNValues[len(littleNValues)-1]+ybuffer ]) #plot axes, buffers added to both limits
+		
+		print("\nplotting graph...\n")
+		plt.show()
+
+		#finish
+		print "plot has been produced"
+
+	def testPlot(self, nplot, littlenplot):
+
+		if(not nplot and not littlenplot):
+			print("\nWhat are you doing, you silly sausage?\nPlot something next time.")
+			return None
+
+		if(not self.allvalues):
+			self.getAllNValues()
+
+		xaxis = range(1,101)
+
+		if(nplot and not littlenplot):
+			self.NPlot()
+		if(littlenplot and not nplot):
+			self.nNPlot()
+		if(nplot and littlenplot):
+
+			littlenvalues = self.getAllLittleNValues()
+
+			fig, ax1 = plt.subplots()
+			ax1.plot(xaxis, self.nvalues[1:], 'b-')
+			ax1.set_xlabel('N Value')
+			ax1.set_ylabel('Cluster Size', color='b')
+			for t1 in ax1.get_yticklabels():
+				t1.set_color('b')
+
+			ax2 = ax1.twinx()
+			ax2.plot(xaxis, littlenvalues, 'r-')
+			ax2.set_ylabel('Number of Clusters Above NX', color='r')
+			for t1 in ax2.get_yticklabels():
+				t1.set_color('r')
+
+			print("\nplotting graph...")
+			plt.show()
